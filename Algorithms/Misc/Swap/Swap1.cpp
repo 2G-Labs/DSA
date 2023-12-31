24289c1b35fefd68c68017b9e24b3d80ee909d51
Afroz Alam <asteroidalaz@gmail.com>
2023-12-31 17:32:34 +0530

    Added SwapAny example

---
+++ Algorithms/Misc/Swap/Swap1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+/**
+ * @brief	Swaps the values of a & b. (Works with any data type, a & b be both of same type)
+ *
+ * @param	a		The reference of first variable to be swapped
+ * @param	b		The reference of second variable to be swapped
+ *
+ */
+template<typename T>
+void swap(T &a, T &b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+int main() {
+
+	// The initial values
+	int a = 19;
+	int b = 34;
+
+	// Printing the values of variables
+	std::cout << "Value of a: " << a << " | Value of b: " << b << std::endl;
+
+
+	// Swapping the values
+	swap(a, b);
+
+	// Printing the values of variables again
+	std::cout << "Value of a: " << a << " | Value of b: " << b << std::endl;
+
+	return 0;
+}
